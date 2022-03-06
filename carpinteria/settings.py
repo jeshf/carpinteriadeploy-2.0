e8fe673079c5520992f8660735fe6e614066c61f
jeshf <engineer_jesushf@outlook.com>
2022-03-05 18:10:44 -0600

    heroku

--- carpinteria/settings.py
+++ carpinteria/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = 'django-insecure-2kuhiv#bzi010c+tzi*6vsb8s%mbakpziw-x%*4s$#fksr)#ma
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
